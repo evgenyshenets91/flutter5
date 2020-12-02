bce897a7aa90fa3c06e73b1c5fbf3a3fa03d263c
Evgeny Shenets <evgenyshenets91@gmail.com>
2020-12-02 23:11:58 +0300

    add initial body with 2 buttons and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Quizzler());
 }
 
-class MyApp extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: Scaffold(
+        backgroundColor: Colors.redAccent,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Center(
+                child: Text('This is where the question text will go',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 25, color: Colors.white)),
+              ),
+            )),
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.all(15),
+          child: FlatButton(
+            onPressed: () {},
+            color: Colors.green,
+            child: Text(
+              'True',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        )),
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.all(15),
+          child: FlatButton(
+            onPressed: () {},
+            color: Colors.blue,
+            child: Text(
+              'False',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ))
+      ],
     );
   }
 }
